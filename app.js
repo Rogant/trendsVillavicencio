5f070c22c0f04b9af3ebe9676e528605dfc8ccdd
Rogant <rogant@gmail.com>
2015-03-26 13:54:50 -0500

    fix

--- app.js
+++ app.js
@@ -13,11 +13,9 @@ var T = new Twit({
 var trends = new Array();
 
 setInterval(function() {
-	var random10 = Math.floor((Math.random() * 10) + 1);
 	var currentdate = new Date(); 
 
 	T.get('trends/place', { id: config.place },  function (err, data, response) {
-
 		var newTrends = _.map(data[0].trends, function(currentObject) {
 			var tmp = _.values(_.pick(currentObject, "name"));
 			return tmp[0];
@@ -26,31 +24,34 @@ setInterval(function() {
 		var tTt = _.difference(newTrends, trends);
 		trends = newTrends;
 
-		var tweetString = '';
-		_.each(tTt, function(data){
-			if (!_.isUndefined(data)) {
-				if((tweetString.length + data.length) < 100){
-					tweetString = tweetString + " '" + data + "'";
-				}
-			}
-		});
-
-		T.get('search/tweets', { q: trends[random10], count: 100 },  function (err, data, response) {
-			var random = Math.floor((Math.random() * data.statuses.length) + 1);
-			var toFav = data.statuses[random];
-
-			if((toFav.user.id != config.userId) && (!toFav.favorited)){
-				T.post('favorites/create', { id: toFav.id_str },  function (err, data, response) {
-					if(err){
-						console.log(err);
-					}else{
-						console.log(currentdate +', favorite: '+ data.id);
-					}
-				});
-			}
-		});
-
 		if(tTt.length > 0){
+			var tweetString = '';
+
+			_.each(tTt, function(data){
+				if (!_.isUndefined(data)) {
+					if((tweetString.length + data.length) < 100){
+						tweetString = tweetString + " '" + data + "'";
+					}
+
+					T.get('search/tweets', { q: data, count: 100, result_type: 'recent' },  function (err, data, response) {
+						var random = Math.floor((Math.random() * data.statuses.length) + 1);
+						var toFav = data.statuses[random];
+
+						if(!_.isUndefined(toFav.favorited)){
+							if((toFav.user.id != config.userId) && (!toFav.favorited)){
+								T.post('favorites/create', { id: toFav.id_str },  function (err, data, response) {
+									if(err){
+										console.log(err);
+									}else{
+										console.log(currentdate +', search favorite: '+ toFav.id_str);
+									}
+								});
+							}
+						}
+					});
+				}
+			});
+
 			T.post('statuses/update', { status: tweetString + config.template }, function(err, data, response) {
 				if(err){
 					console.log(err);
@@ -65,19 +66,21 @@ setInterval(function() {
 
 var stream = T.stream('statuses/filter', { track: config.track })
 stream.on('tweet', function (data) {
-	var random = Math.floor((Math.random() * 100) + 1);
+	var random = Math.floor((Math.random() * 10) + 1);
 
-	if(random == 50){
-		if((data.user.id != config.userId) && (!data.favorited)){
-			T.post('favorites/create', { id: data.id_str },  function (err, data, response) {
-				var currentdate = new Date(); 
+	if(random == 2){
+		if(!_.isUndefined(data.favorited)){
+			if((data.user.id != config.userId) && (!data.favorited)){
+				T.post('favorites/create', { id: data.id_str },  function (err, data, response) {
+					var currentdate = new Date(); 
 
-				if(err){
-					console.log(err);
-				}else{
-					console.log(currentdate +', favorite: '+ data.id);
-				}
-			});
+					if(err){
+						console.log(err);
+					}else{
+						console.log(currentdate +', stream favorite: '+ data.id_str);
+					}
+				});
+			}
 		}
 	}
 });
