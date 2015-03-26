eac2b6d5dbb7f8d5250a5d0ba2373e4beb875f6e
mdsEu <rogant@Andress-MacBook-Pro.local>
2015-03-26 01:56:30 -0500

    parametrisacion

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ var trends = new Array();
 setInterval(function() {
 	var random10 = Math.floor((Math.random() * 10) + 1);
 
-	T.get('trends/place', { id: '23424787' },  function (err, data, response) {
+	T.get('trends/place', { id: 23424848 },  function (err, data, response) {
+
+		console.log(err);
+		console.log(data);
 
 		var newTrends = _.map(data[0].trends, function(currentObject) {
 			var tmp = _.values(_.pick(currentObject, "name"));
@@ -41,15 +44,17 @@ setInterval(function() {
 			console.log('https://twitter.com/'+toFav.user.screen_name+'/status/'+toFav.id_str);
 
 			T.post('favorites/create', { id: toFav.id_str },  function (err, data, response) {
+				console.log(err);
 				//console.log(data.id);
 			});
 		});
 
 		var currentdate = new Date(); 
-		console.log(currentdate+': '+tweetString + ' ahora es una tendencia en #Villavicencio');
+		console.log(currentdate+': '+tweetString + '  is now trending in #India');
 
 		if(tTt.length > 0){
-			T.post('statuses/update', { status: tweetString + ' ahora es una tendencia en #Villavicencio' }, function(err, data, response) {
+			T.post('statuses/update', { status: tweetString + '  is now trending in #India' }, function(err, data, response) {
+				console.log(err);
 				//console.log(data.id);
 			});
 		}
@@ -57,18 +62,18 @@ setInterval(function() {
 }, 60000 );
 
 
-var stream = T.stream('statuses/filter', { track: ['villavicencio', '#villavicencio'] })
+var stream = T.stream('statuses/filter', { track: ['दिल्ली'] })
 //var meta = [ '1.6029', '-74.8751', '4.6403', '-71.0876' ]
 //var stream = T.stream('statuses/filter', { locations: meta })
 
 stream.on('tweet', function (data) {
-	var random = Math.floor((Math.random() * 5) + 1);
-	console.log('random' + random);
+	var random = Math.floor((Math.random() * 100) + 1);
 
-	if(random == 2){
+	if(random == 10){
 		console.log('https://twitter.com/'+data.user.screen_name+'/status/'+data.id_str);
 
 		T.post('favorites/create', { id: data.id_str },  function (err, data, response) {
+			console.log(err);
 			//console.log(data.id);
 		});
 	}
